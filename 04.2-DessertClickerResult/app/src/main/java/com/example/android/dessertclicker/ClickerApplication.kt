403569b6b93354339480b417274c369eeabc9463
icosa-richdeveloper <icosadream@gmail.com>
2020-05-05 00:08:40 +0900

    Add 04.2-DessertClickerResult

---
+++ 04.2-DessertClickerResult/app/src/main/java/com/example/android/dessertclicker/ClickerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.android.dessertclicker
+
+import android.app.Application
+
+class ClickerApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
